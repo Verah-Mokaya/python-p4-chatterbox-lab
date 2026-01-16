ba719392721d3abf17afa391deb2c65a38fce0f6
Verah Mokaya <verahmokaya475@gmail.com>
2026-01-16 16:13:54 +0300

    assiggnemnt done

--- server/app.py
+++ server/app.py
@@ -22,5 +22,69 @@ def messages():
 def messages_by_id(id):
     return ''
 
+@app.route('/messages', methods=['GET', 'POST', 'PATCH', 'DELETE'])
+def messages_handler():
+    if request.method == 'GET':
+        messages = Message.query.all()
+        messages_list = [message.to_dict() for message in messages]
+        response = make_response(
+            jsonify(messages_list),
+            200
+        )
+        return response
+
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            content=data.get('content'),
+            author=data.get('author')
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        response = make_response(
+            jsonify(new_message.to_dict()),
+            201
+        )
+        return response
+
+    elif request.method == 'PATCH':
+        data = request.get_json()
+        message_id = data.get('id')
+        message = Message.query.get(message_id)
+        if message:
+            message.content = data.get('content', message.content)
+            message.author = data.get('author', message.author)
+            db.session.commit()
+            response = make_response(
+                jsonify(message.to_dict()),
+                200
+            )
+            return response
+        else:
+            response = make_response(
+                jsonify({'error': 'Message not found'}),
+                404
+            )
+            return response
+
+    elif request.method == 'DELETE':
+        data = request.get_json()
+        message_id = data.get('id')
+        message = Message.query.get(message_id)
+        if message:
+            db.session.delete(message)
+            db.session.commit()
+            response = make_response(
+                jsonify({'message': 'Message deleted'}),
+                200
+            )
+            return response
+        else:
+            response = make_response(
+                jsonify({'error': 'Message not found'}),
+                404
+            )
+            return response
+
 if __name__ == '__main__':
     app.run(port=5555)
